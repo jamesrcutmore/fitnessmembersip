190fb2a50232b556ee304a6ac5c917f1450d8270
jamescutmore <95052094+jamesrcutmore@users.noreply.github.com>
2022-11-24 11:07:40 +0000

    member model

--- members/models.py
+++ members/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class Member(models.Model):
+    firstname = models.CharField( max_length=50)
+    lastname = models.CharField( max_length=50)
+    age = models.CharField( max_length=3)
+    email = models.CharField( max_length=30)
+    user = models.ForeignKey( settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at= models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.firstname
+    
