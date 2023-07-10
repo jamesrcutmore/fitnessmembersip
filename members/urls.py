e718ff21c7ba4ed0ef9d57729320ff0d54e68d02
James cutmore <robcutmore36@gmail.com>
2023-07-10 15:54:56 +0000

    url.py .added product & views.py added product url

--- members/urls.py
+++ members/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/',views.about, name='about'),
+    path('products/',views.products, name='products'),
     path('services/',views.services, name='services'),
     path('login/',views.login, name='login'),
     path('register/',views.register, name='register'),
--- members/views.py
+++ members/views.py
@@ -2,17 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
-    return render(request,'members/home.html')
+    return render(request, 'members/home.html')
+
 
 def about(request):
-    return render(request,'members/about.html')
+    return render(request, 'members/about.html')
+
 
 def services(request):
-    return render(request,'members/services.html')
+    return render(request, 'members/services.html')
+
+
+def products(request):
+    return render(request, 'members/products.html')
+
 
 def login(request):
-    return render(request,'members/login.html')
+    return render(request, 'members/login.html')
+
 
 def register(request):
-    return render(request,'members/register.html')
+    return render(request, 'members/register.html')
